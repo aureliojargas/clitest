967b2c25e692ea0042763b84bb217d914babe9c0
Aurelio Jargas <verde@aurelio.net>
2013-07-26 11:32:39 -0300

    extra processing moved out of the main function

--- doctest.sh
+++ doctest.sh
@@ -195,15 +195,6 @@ _process_test_file ()
 
 	# Run pending tests
 	test -n "$test_command" && _run_test "$test_command"
-
-	if test $nr_file_tests -eq 0
-	then
-		_message "$(basename "$0"): Error: no test found in input file: $test_file"
-		exit 1
-	fi
-
-	# Append file stats to global holder
-	files_stat_message=$(printf '%s\n%s' "$files_stat_message" "$(_get_file_stats)")
 }
 
 # Do not change these vars
@@ -248,7 +239,18 @@ do
 	#
 	sed "s/$(printf '\r')$//" "$test_file" > "$temp_file"
 
+	# The magic happens here
 	_process_test_file "$temp_file"
+
+	# Abort when no test found
+	if test $nr_file_tests -eq 0
+	then
+		_message "$(basename "$0"): Error: no test found in input file: $test_file"
+		exit 1
+	fi
+
+	# Append file stats to global holder
+	files_stat_message=$(printf '%s\n%s' "$files_stat_message" "$(_get_file_stats)")
 done
 
 _clean_up
