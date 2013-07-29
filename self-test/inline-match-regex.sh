5ff2dcc85e79a75b01431801fb224c271a7537ff
Aurelio Jargas <verde@aurelio.net>
2013-07-29 15:25:30 -0300

    Comments realigned

--- self-test/inline-match-regex.sh
+++ self-test/inline-match-regex.sh
@@ -12,36 +12,36 @@
 
 # Use anchors ^ and $ to match the full output text
 
-$ echo 'abc123'           #→ --regex ^abc123$
-$ echo 'abc123'           #→ --regex ^abc.*3$
-$ echo 'abc123'           #→ --regex ^abc[0-9]+$
+$ echo 'abc123'                 #→ --regex ^abc123$
+$ echo 'abc123'                 #→ --regex ^abc.*3$
+$ echo 'abc123'                 #→ --regex ^abc[0-9]+$
 
 # Omit one or both anchors to make a parcial match
 
-$ echo 'abc123'           #→ --regex ^abc
-$ echo 'abc123'           #→ --regex 123$
-$ echo 'abc123'           #→ --regex [0-9]+$
-$ echo 'abc123'           #→ --regex bc
-$ echo 'abc123'           #→ --regex .
+$ echo 'abc123'                 #→ --regex ^abc
+$ echo 'abc123'                 #→ --regex 123$
+$ echo 'abc123'                 #→ --regex [0-9]+$
+$ echo 'abc123'                 #→ --regex bc
+$ echo 'abc123'                 #→ --regex .
 
 # Blanks are preserved, no escaping needed
 
-$ echo 'abc 123'          #→ --regex ^abc [0-9]+$
+$ echo 'abc 123'                #→ --regex ^abc [0-9]+$
 
 # In some systems, the special sequence \t is expanded to a tab in
 # egrep regexes. You'll need to test in your system if that's the
 # case. I recommend using a literal tab to avoid problems.
 
-$ printf 'may\tfail'      #→ --regex ^may\tfail$
-$ printf 'may\tfail'      #→ --regex ^may[\t]fail$
-$ printf 'will\tmatch'    #→ --regex ^will	match$
+$ printf 'may\tfail'            #→ --regex ^may\tfail$
+$ printf 'may\tfail'            #→ --regex ^may[\t]fail$
+$ printf 'will\tmatch'          #→ --regex ^will	match$
 
 # Since it's an egrep test, regexes are not multiline.
 # You can only match a single line.
 # These tests will fail:
 
-$ printf 'will\nfail'     #→ --regex will.*fail
-$ printf 'will\nfail'     #→ --regex will\nfail
+$ printf 'will\nfail'           #→ --regex will.*fail
+$ printf 'will\nfail'           #→ --regex will\nfail
 
 # Syntax: Must be exactly one space before and after --regex
 
