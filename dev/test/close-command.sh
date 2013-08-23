477980ae97dee299e1e1f61a384e9c10aa9fd527
Aurelio Jargas <verde@aurelio.net>
2013-08-22 23:03:26 -0300

    Add comments

--- dev/test/close-command.sh
+++ dev/test/close-command.sh
@@ -1,15 +1,22 @@
+# Syntax: Empty prompt (with space) closes the previous command
+
 $ echo 1
 1
 $ 
+
+# Syntax: Empty prompt (no space) closes the previous command
+
 $ echo 2
 2
 $
+
+# Syntax: Repeated empty prompts are OK
+
 $ 
 $
 $
 
-# Comments are OK after a closed command.
-# They're just ignored.
+# Syntax: End-of-file closes the last command
 
 $ echo 3
 3
